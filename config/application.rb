21352917d30f19380aa39ddaff0da1d761aefa78
buitra210 <buitra210ne@gmail.com>
2025-04-04 10:59:32 +0700

    feat-86369/init-project

---
+++ config/application.rb
@@ -0,0 +1,35 @@
+require_relative "boot"
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsTutorial
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+    config.active_storage.variant_processor = :mini_magick
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    # Load config/settings.yml
+    config.before_configuration do
+      env_file = Rails.root.join("config", "settings.yml")
+      if File.exist?(env_file)
+        config_settings = YAML.load_file(env_file)[Rails.env] || {}
+        config_settings.each do |key, value|
+          ENV[key.to_s.upcase] ||= value.to_s
+        end
+      end
+    end
+  end
+end
